f2366fbe309ad1c80727ccbe42ccb702ac121dd8
prasanna7codes <prasannalit098@gmail.com>
2025-05-14 22:57:27 +0530

    logging the url,method and the timestamp on the access to my http server

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const fs = require('fs');
 
 app.use((req, res, next) => {
-    //  vvv Changed lines vvv
+    
     console.log(`URL: ${req.url}`);
     console.log(`Method: ${req.method}`);
     const date = new Date();
     console.log(`Timestamp: ${date.toISOString()}`);
-    //  ^^^ Changed lines ^^^
+
     next();
 });
 
@@ -18,8 +18,4 @@ app.get('/ride1', function (req, res) {
     });
 });
 
-app.listen(3001, () => {
-    //  vvv Added line vvv
-    console.log("Server is running on port 3001");
-    //  ^^^ Added line ^^^
-});
+app.listen(3001);
