534e31bd1067126bffbeaf3c272da109692864ce
prasanna7codes <prasannalit098@gmail.com>
2025-05-15 21:16:59 +0530

    creating a miidleware to see if a persoj is logged in or not

--- index.js
+++ index.js
@@ -1,21 +1,112 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "ramdomharkiratilovekiara"
 const app = express();
-const fs = require('fs');
+app.use(express.json());
 
-app.use((req, res, next) => {
-    
-    console.log(`URL: ${req.url}`);
-    console.log(`Method: ${req.method}`);
-    const date = new Date();
-    console.log(`Timestamp: ${date.toISOString()}`);
+const users = [];
 
-    next();
-});
+app.post("/signup", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.get('/ride1', function (req, res) {
+    users.push({
+        username: username,
+        password: password
+    })    
+
     res.json({
-        message: "you have riden the ride1"
-    });
-});
+        message: "You are signed up"
+    })
 
-app.listen(3001);
+    console.log(users)
+    
+})
+
+
+app.post("/signin", function(req, res) {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+
+    
+    let foundUser = null;
+
+    for (let i = 0; i<users.length; i++) {
+        if (users[i].username == username && users[i].password == password) {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser) {
+        const token = jwt.sign({
+            username: username,
+       }, JWT_SECRET) ;
+
+        
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password"
+        })
+    }
+    console.log(users)
+})
+
+
+
+
+function auth(req,res,next){
+    const token = req.headers.token // jwt
+    const decodedInformation = jwt.verify(token, JWT_SECRET);  // returns an object bcoz we encoded an object in the first place
+
+    if(decodedInformation.username){
+
+        req.username=decodedInformation.username;
+        next();
+    }
+
+    else{
+        res.json({
+            message : "you are not loged in "
+        })
+    }
+
+
+
+    
+    
+}
+
+
+app.get("/get-password",auth, function(req, res) {
+    
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === req.username)  {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.json({
+            message: "user not found"
+        })
+    }
+
+
+})
+
+
+app.listen(3001)
+
+
+
